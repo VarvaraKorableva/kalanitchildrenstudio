f0ff8c171ff366cceeae0664d7064968b9366501
Varvara Korableva <v.korableva1990@yandex.ru>
2025-01-31 23:10:34 +0200

    added header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,39 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <section className="header__section">
+            <div className="header__wrapper">
+            <div className="header__logo-container">
+                <div className="header__logo-pic-container">
+                    <div className="header__logo-pic"></div>
+                </div>
+                <h3 className="header__logo-text">Каланит Клита</h3>
+            </div>
+
+            <ul className='header__nav-container'>
+            <li className='header__link'>
+                    <p className='header__link-title'>О нас</p>
+                </li>
+                <li className='header__link'>
+                    <p className='header__link-title'>Расписание</p>
+                </li>
+                <li className='header__link'>
+                    <p className='header__link-title'>Фотографии</p>
+                </li>
+                <li className='header__link'>
+                    <p className='header__link-title'>Контакты</p>
+                </li>
+            </ul>
+            </div>
+
+            <div className='header__grass-container'>
+                <div className='header__grass'></div>
+                <div className='header__grass'></div>
+                <div className='header__grass'></div>
+            </div>
+        </section>
+    )
+}
+
+export default Header
